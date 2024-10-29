b460f4dab4aae8407d59384dbfbc07abe28d2dfb
kratos155 <mehdi.zinelabidine@esprit.tn>
2024-10-29 14:54:28 +0100

    front activite

--- activite/views.py
+++ activite/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Activite
 
-# Create your views here.
+def front_view(request):
+    activities = Activite.objects.all()  # Get all activities
+    return render(request, 'index.html', {'activities': activities})
